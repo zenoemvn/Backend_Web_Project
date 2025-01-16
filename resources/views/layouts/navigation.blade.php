4448a20b32ebd89d51857ac2672e680b7e4e2391
zenoemvn <vanneygenzeno@gmail.com>
2025-01-16 01:37:52 +0100

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,9 +5,9 @@
             <div class="flex">
                 <div class="shrink-0 flex items-center">
                 <a href="{{ auth()->check() ? (auth()->user()->usertype === 'admin' ? route('admin.dashboard') : route('dashboard')) : route('home') }}">
-    Home
+    
 </a>
-w
+
 
     <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
 </a>
